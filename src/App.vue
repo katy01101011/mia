f20c1d568b014e03e8330be44b64f7c1e5a23a4c
katy01101011 <katy.91560@gmail.com>
2024-05-26 09:22:14 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <mia-component></mia-component>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import MiaComponent from "./components/MiaComponent.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    MiaComponent,
   },
 };
 </script>
@@ -22,5 +21,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  background: lightpink;
 }
 </style>
---
+++ src/components/MiaComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="text-uppercase d-flex">
+    <div class="foto">
+      <img style="width: 100%" src="../assets/mia-kamil.jpeg">
+    </div>
+    <h1>il mio primo sito!</h1>
+    <h1>mia</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.text-uppercase {
+  text-transform: uppercase;
+  color: white;
+}
+.foto {
+  width: 60vw;
+  border-radius: 30px;
+  overflow: hidden;
+}
+.d-flex {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
